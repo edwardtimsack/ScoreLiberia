502e849bcc3b976107dec05bbe4190514e94d2b1
edwardtimsack <edwardtimothysackie@gmail.com>
2023-09-08 04:44:48 -0400

    completed search and sort function

--- app.js
+++ app.js
@@ -18,19 +18,36 @@ let lastArticle = document.querySelector('.last_article')
 let navI = document.querySelectorAll('.nav i');
 let body = document.querySelector('body');
 let tableIcon = document.querySelectorAll('.table i');
- 
-// league table 
-// let rank = document.querySelector('.rank');
-// let logo = document.querySelectorAll('.pics');
-// let teamName = document.querySelector('.team-name');
-// let win = document.querySelector('.win');
-// let draw = document.querySelector('.draw');
-// let lose = document.querySelector('.lose');
-// let gamePlayed = document.querySelector('.match-played');
-// let point = document.querySelector('.point');
-// leagueTitle.innerHTML = "League Table"
 let target = document.querySelector('.teams')
 const currentMatch = document.querySelector('.today_match');
+let searchInput = document.querySelector('input');
+let ascendingAlphabetical = document.querySelector('#first');
+let decendingAlphabetical = document.querySelector('#last');
+
+let positionss = document.querySelector('#position')
+let point = document.querySelector('.nav #point');
+let sortLeagues = document.querySelector('#Sort_leagues');
+
+sortLeagues.addEventListener('change' , function(){
+    const selectedOption = sortLeagues.value;
+    if(selectedOption === point.value){
+    console.log('pointttttttt');
+    sortTableByPointsAsc()
+    }
+    else if(selectedOption === positionss.value){
+        console.log('positionsssssssss');
+        sortTableByPositionDesc()
+        }
+        else if(selectedOption === decendingAlphabetical.value){
+            console.log('descending')
+            sortTableByClubNameDesc()
+        }
+        else if(selectedOption === ascendingAlphabetical.value){
+            console.log('ascending');
+            sortTableByClubNameAsc()
+            }
+    console.log(selectedOption);
+});
 
 const leagueTable = [
     {
@@ -175,24 +192,114 @@ const leagueTable = [
     }
 ]
 
+// sort function 
+// Define global variables to keep track of the current sorting order
+let sortByPointsAsc = false;
+let sortByPointsDesc = false;
 
-const Fixtures = [
+// Function to sort the league table by points in ascending order
+function sortTableByPointsAsc() {
+    leagueTable.sort((a, b) => a.points - b.points);
+    displayLeague(); // Update the table on the page
+    sortByPointsAsc = true;
+    sortByPointsDesc = false;
+}
+
+// Function to sort the league table by points in descending order
+function sortTableByPointsDesc() {
+    leagueTable.sort((a, b) => b.points - a.points);
+    displayLeague(); // Update the table on the page
+    sortByPointsAsc = false;
+    sortByPointsDesc = true;
+}
+
+// sortPostion Function
+function sortTableByPositionDesc() {
+    leagueTable.sort((a,b) => b.position - a.position);
+    displayLeague()
+    sortByPointsAsc = false;
+    sortByPointsDesc = true;
+}
+
+// Function to sort the league table by club name in ascending order
+function sortTableByClubNameAsc() {
+    leagueTable.sort((a, b) => a.clubName.localeCompare(b.clubName));
+    displayLeague(); // Update the table on the page
+}
+
+// Function to sort the league table by club name in descending order
+function sortTableByClubNameDesc() {
+    leagueTable.sort((a, b) => b.clubName.localeCompare(a.clubName));
+    displayLeague(); // Update the table on the page
+}
+
+// Add event listeners to your sorting buttons or UI elements
+// const sortClubNameAscButton = document.getElementById('sortClubNameAscButton');
+// sortClubNameAscButton.addEventListener('click', sortTableByClubNameAsc);
+
+// const sortClubNameDescButton = document.getElementById('sortClubNameDescButton');
+// sortClubNameDescButton.addEventListener('click', sortTableByClubNameDesc);
+
+
+
+// Add event listeners to your sorting buttons or UI elements
+// const sortAscButton = document.getElementById('sortAscButton');
+// sortAscButton.addEventListener('click', sortTableByPointsAsc);
+
+// const sortDescButton = document.getElementById('sortDescButton');
+// sortDescButton.addEventListener('click', sortTableByPointsDesc);
+
+// Update your displayLeague function to include sorting criteria
+// function displayLeague() {
+//     // ... your existing code for displaying the table
+
+//     // Check the sorting criteria and display an indicator
+//     if (sortByPointsAsc) {
+//         // Display an indicator for ascending order (e.g., an up arrow)
+//         sortAscButton.classList.add('active');
+//         sortDescButton.classList.remove('active');
+//     } else if (sortByPointsDesc) {
+//         // Display an indicator for descending order (e.g., a down arrow)
+//         sortAscButton.classList.remove('active');
+//         sortDescButton.classList.add('active');
+//     } else {
+//         // No sorting indicator when not sorted
+//         sortAscButton.classList.remove('active');
+//         sortDescButton.classList.remove('active');
+//     }
+// }
+
+
+
+
+
+
+// const byPosition = leagueTable.sort((a,b) => {
+//     return b.position - a.position;
+// });
+// console.log(byPosition)
+
+
+ const Fixtures = [
     {
         id: 1,
         date: '2023-06-02',
         time: '3:00pm',
         stadium: "Samuel Kanyon Doe Sports Stadium",
+        statuses: ':',
         homeTeam: {
             teamName: 'LISCR',
             score: 3,
             playerScore: 'Jim',
             playerAssist: 'Tim',
+            logo: './img/LISCR FC.png'
         },
         awayTeam: {  // Corrected placement of awayTeam
-            teamName: 'BYC',
+            teamName: 'Freeport',
             score: 2,
             playerScore: 'James',
-            playerAssist: 'Ben'
+            playerAssist: 'Ben',
+            logo: './img/Freeportt.png'
         },
         leagueName: 'LFA'
     },
@@ -201,17 +308,20 @@ const Fixtures = [
         date: '2023-06-04',
         time: '1:00pm',
         stadium: "Samuel Kanyon Doe Sports Stadium",
+        statuses: ':',
         homeTeam: {
-            teamName: 'Bea Mount',
+            teamName: 'Sandi',
             score: 1,
             playerScore: 'Oscar',
             playerAssist: 'Albin',
+            logo: './img/Sandi.png'
         },
         awayTeam: {  // Corrected placement of awayTeam
             teamName: 'Jubilee',
             score: 3,
             playerScore: 'Nathan',
-            playerAssist: 'James'
+            playerAssist: 'James',
+            logo: './img/Jubile.png'
         },
         leagueName: 'LFA'
     },
@@ -220,48 +330,43 @@ const Fixtures = [
         date: '2023-06-04',
         time: '4:00pm',
         stadium: "Antionette Tubman Stadium",
+        statuses: ':',
         homeTeam: {
-            teamName: 'Wantaga',
+            teamName: 'Muscat',
             score: 0,
             playerScore: 'Ben',
             playerAssist: 'Dave',
+            logo: './img/Muscat1.png'
         },
         awayTeam: {  // Corrected placement of awayTeam
-            teamName: 'BYC',
+            teamName: 'Wantaga',
             score: 2,
             playerScore: 'Albin',
-            playerAssist: 'Emmanuel'
+            playerAssist: 'Emmanuel',
+            logo: './img/Wantaga1.png'
         },
         leagueName: 'LFA'
-    },
-    {
-        id: 4,
-        date: '2023-06-07',
-        time: '6:20pm',
-        stadium: "Samuel Kanyon Doe Sports Stadium",
-        homeTeam: {
-            teamName: 'Nimba United',
-            score: 3,
-            playerScore: 'Tom',
-            playerAssist: 'Tuazama',
-        },
-        awayTeam: {  // Corrected placement of awayTeam
-            teamName: 'Cece United',
-            score: 2,
-            playerScore: 'Leon',
-            playerAssist: 'Bess'
-        },
-        leagueName: 'LFA'
-    },
+    }
+    
 
 ];
 
+function lfaFixture() {
+
+}
+
 
 function displayLeague() {
+
+    target.innerHTML = '';
+
     for (let i = 0; i < leagueTable.length; i++) {
+        const searchQuery = searchInput.value.toLowerCase()
+        if (leagueTable[i].clubName.toLowerCase().includes(searchQuery.toLowerCase())) {
+
         const aboutTeam = document.createElement('div');
         const ranks = document.createElement('div');
-        const teamLogo = document.createElement('div');
+        const teamLogo = document.createElement('img');
         const teamNames = document.createElement('div');
         const win = document.createElement('div');
         const loss = document.createElement('div');
@@ -271,6 +376,7 @@ function displayLeague() {
 
         aboutTeam.classList.add('about');
         ranks.innerHTML = leagueTable[i].position;
+        // teamLogo.src = leagueTable[i].clubLogo;
         teamNames.innerHTML = leagueTable[i].clubName;
         win.innerHTML = leagueTable[i].gameWon;
         draw.innerHTML = leagueTable[i].gameDrawn;
@@ -279,6 +385,7 @@ function displayLeague() {
         gamePoints.innerHTML = leagueTable[i].points;
 
         aboutTeam.appendChild(ranks);
+        // aboutTeam.appendChild(teamLogo)
         aboutTeam.appendChild(teamNames);
         aboutTeam.appendChild(win);
         aboutTeam.appendChild(draw)
@@ -287,24 +394,32 @@ function displayLeague() {
         aboutTeam.appendChild(gamePoints);
 
         target.appendChild(aboutTeam);
-        console.log(leagueTable[i].position)
+        // console.log(aboutTeam)
+        // console.log(leagueTable[i].position)
+        // console.log(leagueTable[i].teamLogo)
+        }
+    
     }
 }
-
+// Add an event listener to the search input field
+const teamSearch = document.getElementById('teamSearch');
+teamSearch.addEventListener('input', function () {
+    const searchQuery = this.value;
+    displayLeague(searchQuery);
+});
 displayLeague();
 
 
-function onGoingMatch() {
-    const todayLeague = document.querySelector('')
-}
 
 const lfaBox = document.querySelector('.today_match')
-function lfaDIvision() {
+
+
+function lfaDivision() {
     const me = document.createElement('h1');
-    me.textContent = "hello";
-    today_match.appendChild(me);
     const lfaDiv = document.createElement('div');
-    const lfaLogo = document.createElement('div');
+    const lfaLogo = document.createElement('img');
+    lfaLogo.src = './img/LFA.png';
+    lfaDiv.appendChild(lfaLogo)
     const leag = document.createElement('div');
     const leagName = document.createElement('h1');
     const leagMatch = document.createElement('p');
@@ -317,70 +432,124 @@ function lfaDIvision() {
     leag.classList.add('leag')
     leag.appendChild(leagName);
     leag.appendChild(leagMatch);
-    lfaBox.appendChild(lfaDiv)
+    lfaDiv.appendChild(leag);
+    lfaBox.appendChild(lfaDiv);
+
+   
+        for(let t = 0; t < Fixtures.length; t++){
+            const fingers = document.createElement('div');
+            const homeTeamlogo = document.createElement('img');
+            const homeTeamName = document.createElement('h1');
+            const homeTeamScore = document.createElement('p');
+            const divider = document.createElement('p');
+            const awayTeamScore = document.createElement('p');
+            const awayTeamLogo = document.createElement('img');
+            const awayTeamName = document.createElement('h1');
+
+            homeTeamlogo.src = Fixtures[t].homeTeam.logo;
+            homeTeamName.innerHTML = Fixtures[t].homeTeam.teamName;
+            homeTeamScore.innerHTML = Fixtures[t].homeTeam.score;
+            divider.innerHTML = Fixtures[t].statuses;
+            awayTeamScore.innerHTML = Fixtures[t].awayTeam.score;
+            awayTeamLogo.src = Fixtures[t].awayTeam.logo;
+            awayTeamName.innerHTML = Fixtures[t].awayTeam.teamName;
+
+            fingers.classList.add('table')
+
+
+            fingers.appendChild(homeTeamlogo);
+            fingers.appendChild(homeTeamName);
+            fingers.appendChild(homeTeamScore);
+            fingers.appendChild(divider);
+            fingers.appendChild(awayTeamScore);
+            fingers.appendChild(awayTeamLogo);
+            fingers.appendChild(awayTeamName);
+            lfaBox.appendChild(fingers)
+
+
+
+            // console.log(divider);
+            // console.log(homeTeamScore);
+            // console.log(awayTeamScore);
+            // console.log(homeTeamlogo);
+            // console.log(awayTeamLogo);
+            // console.log(homeTeamName);
+            // console.log(awayTeamName);
+
+            console.log(Fixtures[t].awayTeam.logo);
+           
+            
+
+
+
+            
+
+        }
+}
+    lfaDivision();
 
     
-}
+// const teamSearchInput = document.getElementById('teamSearch');
+// const searchResultsContainer = document.getElementById('searchResults');
+// teamSearchInput.addEventListener('input', function () {
+//     const inputArea = teamSearchInput.value.toLowerCase();
 
+//     // Clear the previous search results
+//     searchResultsContainer.innerHTML = '';
 
-// function createOption(){
-//     // const leagueTitle = document.createElement('h1');
-//     const aboutTeam = document.createElement('div')
-//     const ranks = document.createElement('div');
-//     const teamLogo = document.createElement('div');
-//     const teamNames = document.createElement('div');
-//     const win = document.createElement('div');
-//     const loss = document.createElement('div');
-//     const draw = document.createElement('div')
-//     const gamePlayed = document.createElement('div');
-//     const gamePoints = document.createElement('div');
+//      // Check if the input area is empty
+//      if (inputArea === '') {
+//         return; // Exit the function and do not display any results
+//     }
+//     // Iterate through the leagueTable array and check for matching team names
+//     for (let i = 0; i < leagueTable.length; i++) {
+//         const team = leagueTable[i];
+//         const teamName = team.clubName.toLowerCase();
 
-//         aboutTeam.classList.add('about')
-//         // leagueTitle.innerHTML = 'League Table';
-//         ranks.innerHTML = leagueTable[0].position;
-//         teamNames.innerHTML = leagueTable[0].clubName;
-//         win.innerHTML = leagueTable[0].gameWon;
-//         draw.innerHTML = leagueTable[0].gameDrawn;
-//         loss.innerHTML = leagueTable[0].gameLoss
-//         gamePlayed.innerHTML = leagueTable[0].matchPlayed;
-//         gamePoints.innerHTML = leagueTable[0].points
-//         // console.log(ranks)
-
-//     aboutTeam.appendChild(ranks);
-//     aboutTeam.appendChild(teamNames);
-//     aboutTeam.appendChild(win);
-//     aboutTeam.appendChild(draw);
-
-//     aboutTeam.appendChild(loss);
-//     aboutTeam.appendChild(gamePlayed);
-//     aboutTeam.appendChild(gamePoints)
-//     console.log(aboutTeam)
-//     target.appendChild(aboutTeam)
-// }
-// createOption()
+//         if (teamName.includes(inputArea)) {
+//             // Create a new div element for the matching team
+//             const teamElement = document.createElement('div');
+//             teamElement.classList.add('search-result');
+//             teamElement.textContent = team.clubName;
+//             searchResultsContainer.appendChild(teamElement);
+//         }
+//     }
+//     // Check if no matching teams were found
+//     if (searchResultsContainer.children.length === 0) {
+//         const noResultsElement = document.createElement('div');
+//         noResultsElement.textContent = 'No matching teams found.';
+//         searchResultsContainer.appendChild(noResultsElement);
+//     }
+// });
 
 
 
-switchTheme.addEventListener('click' , function() {
-    body.style.backgroundColor = 'white';
-    // tableIcon.style.color = 'black'
-    wrapper.style.backgroundColor = 'rgb(245, 241, 241)';
-    aside.style.backgroundColor = 'white';
-    aside.style.color = 'black';
-    asideTitle.style.color = 'black';
-    league.style.color = 'black';
-    league1.style.color = 'black';
 
-    liveMatches.style.backgroundColor = 'white';
-    liveMatches.style.color = 'black';
-    liveButton.style.color = 'white';
-    todayMatch.style.color = 'black';
-    todayMatch.style.backgroundColor = 'white';
-    lastArticle.style.backgroundColor = 'white';
-    greetings.style. color = 'black';
+//    const clubs = leagueTable[0].clubName;
+//         console.log(clubs)
+    
 
 
-})
+// switchTheme.addEventListener('click' , function() {
+//     body.style.backgroundColor = 'white';
+//     // tableIcon.style.color = 'black'
+//     wrapper.style.backgroundColor = 'rgb(245, 241, 241)';
+//     aside.style.backgroundColor = 'white';
+//     aside.style.color = 'black';
+//     asideTitle.style.color = 'black';
+//     league.style.color = 'black';
+//     league1.style.color = 'black';
+
+//     liveMatches.style.backgroundColor = 'white';
+//     liveMatches.style.color = 'black';
+//     liveButton.style.color = 'white';
+//     todayMatch.style.color = 'black';
+//     todayMatch.style.backgroundColor = 'white';
+//     lastArticle.style.backgroundColor = 'white';
+//     greetings.style. color = 'black';
+
+
+// })
 
 // console.log(leagueTable[5].clubName)
 // const clubLogoUrl = leagueTable[8].clubLogo;
